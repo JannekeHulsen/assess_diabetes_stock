673728a9d362adfe8d574218c10a6d392ce46a52
JannekeHulsen <jannekehulsen@gmail.com>
2024-11-13 20:40:58 +0100

    Add home page

---
+++ project/Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.markdown("# How many diabetes supplies do I need?")
+
+"""
+I've got 8 infusion set, 11 reservoirs and 3 pump batteries left.
+
+- How many do I need to order to cover my needs for the next three months?
+- Until when can I sustain on my current stock?
+- How many articles do I need to bring with me on holiday?
+
+With this web app, I've tried to create a tool for at least myself and hopefully others to quickly make the calculations I need.
+
+Try it out!
+
+Navigate to the side bar to check out:
+- **Assess stock**: For most of your stock supply and order questions.
+- **When order next**: What is the next date you need to order?
+- **Items overview**: An overview of the diabetes supplies that are currently covered by this app.
+
+Please let me know what functionality you are missing through a [github issue](https://github.com/JannekeHulsen/assess_diabetes_stock/issues).
+In the future, I'll add other ways for contact :)
+"""
+# TODO reference to subpages link in list above.
+# TODO more concrete questions for assess stock.
+# TODO formulier toevoegen die email stuurt.
